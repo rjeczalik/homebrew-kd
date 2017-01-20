e1fd98bf1ab512bdac4780894917dc0086ab850b
Rafal Jeczalik <rjeczalik@gmail.com>
2017-01-20 21:44:13 +0100

    formula: sha1 -> sha256

--- Formula/kd.rb
+++ Formula/kd.rb
@@ -3,7 +3,7 @@ class Kd < Formula
   homepage 'https://koding.com'
 
   url 'https://s3.amazonaws.com/koding-kd/development/kd.darwin_amd64.gz'
-  sha1 '5f3d19c562f548ce56af1c7dbb43399a85071006e093fd730cd44ffaa98b34dc'
+  sha256 '5f3d19c562f548ce56af1c7dbb43399a85071006e093fd730cd44ffaa98b34dc'
   version '0.0.1'
 
   def install
