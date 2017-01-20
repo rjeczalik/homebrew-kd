061b27579f0af3cbcd0a8df44fc64e47f6dbe9c9
Rafal Jeczalik <rjeczalik@gmail.com>
2017-01-20 22:06:08 +0100

    formula: add --devel channel

--- Formula/kd.rb
+++ Formula/kd.rb
@@ -2,11 +2,14 @@ class Kd < Formula
   desc 'Command line interface to Koding.'
   homepage 'https://koding.com'
 
-  url 'https://s3.amazonaws.com/koding-kd/development/kd.darwin_amd64.gz'
-  sha256 '5f3d19c562f548ce56af1c7dbb43399a85071006e093fd730cd44ffaa98b34dc'
+  url 'https://s3.amazonaws.com/koding-kd/production/kd.darwin_amd64.gz'
   version '0.0.1'
 
+  devel do
+    url 'https://s3.amazonaws.com/koding-kd/development/kd.darwin_amd64.gz'
+  end
+
   def install
-	  bin.install 'kd.darwin_amd64'
+    bin.install 'kd.darwin_amd64'
   end
 end
