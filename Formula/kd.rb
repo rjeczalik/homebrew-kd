d96c2a4332d495883f500585d99abe165b3e6406
Rafal Jeczalik <rjeczalik@gmail.com>
2017-03-24 21:10:36 +0100

    formula: bump to 193

--- Formula/kd.rb
+++ Formula/kd.rb
@@ -6,12 +6,12 @@ class Kd < Formula
   version '0.0.2'
 
   devel do
-    url 'https://s3.amazonaws.com/koding-kd/development/kd-0.1.191.darwin_amd64.gz'
-    version '0.1.191'
+    url 'https://s3.amazonaws.com/koding-kd/development/kd-0.1.193.darwin_amd64.gz'
+    version '0.1.193'
   end
 
   def install
-    mv 'kd-0.1.191.darwin_amd64', 'kd'
+    mv 'kd-0.1.193.darwin_amd64', 'kd'
     bin.install 'kd'
   end
 end
